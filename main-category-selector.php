04f6618b0bbb94fc3a2db90293c9d995b304b2c1
Diego Giraldo <diego.dgiraldo36@gmail.com>
2021-04-26 16:17:16 -0500

    Add definitions

--- main-category-selector.php
+++ main-category-selector.php
@@ -13,3 +13,9 @@
 if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
+
+// Definitions.
+define( 'MAINCATSEL_VERSION', '0.0.1' );
+define( 'MAINCATSEL_DIR', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
+define( 'MAINCATSEL_URL', untrailingslashit( plugins_url( basename( plugin_dir_path( __FILE__ ) ), basename( __FILE__ ) ) ) );
+
