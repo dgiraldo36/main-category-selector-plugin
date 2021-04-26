bca316ea966a7a52bd5b4fd66c2eb35cc9d43f69
Diego Giraldo <diego.dgiraldo36@gmail.com>
2021-04-26 16:18:14 -0500

    Create main class and instantiate

--- main-category-selector.php
+++ main-category-selector.php
@@ -19,3 +19,46 @@ define( 'MAINCATSEL_VERSION', '0.0.1' );
 define( 'MAINCATSEL_DIR', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 define( 'MAINCATSEL_URL', untrailingslashit( plugins_url( basename( plugin_dir_path( __FILE__ ) ), basename( __FILE__ ) ) ) );
 
+if ( ! class_exists('MAINCATSEL') ) {
+class MAINCATSEL {
+
+    /**
+     * The single instance of the class.
+     *
+     * @var self
+     * @since  1.0.0
+     */
+    private static $instance = null;
+
+    /**
+     * Allows for accessing single instance of class. Class should only be constructed once per call.
+     *
+     * @since  1.0.0
+     * @static
+     * @return self Main instance.
+     */
+    public static function instance() {
+        if ( is_null( self::$instance ) ) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function __construct() {
+    }
+}
+}
+
+/**
+ * Main instance.
+ *
+ * Returns the main instance of Custom CSV Importer for WP Job Manager to prevent the need to use globals.
+ *
+ * @since  1.0.0
+ * @return CJM_Importer
+ */
+function MAINCATSELINST() { // phpcs:ignore WordPress.NamingConventions.ValidFunctionName
+    return MAINCATSEL::instance();
+}
+
+$GLOBALS['maincatesel'] = MAINCATSELINST();
