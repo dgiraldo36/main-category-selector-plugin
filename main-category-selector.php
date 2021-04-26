57baad3513a78e6423e35fb78833b59d1a774463
Diego Giraldo <diego.dgiraldo36@gmail.com>
2021-04-26 16:16:25 -0500

    Add plugin boilerplate

---
+++ main-category-selector.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Main Category Selector Plugin
+ * Description: Allows to define a main category for Posts and Custom Post Types.
+ * Version: 0.0.1
+ * Author: Diego Giraldo
+ * Author URI: https://dgiraldo.co
+ * Developer: Diego Giraldo
+ * Developer URI: http://dgiraldo.co/
+ * Text Domain: main-cat-selector
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
