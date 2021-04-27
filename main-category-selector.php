176205c600f8418a7bec383111ae5e0ec917ec60
Diego Giraldo <diego.dgiraldo36@gmail.com>
2021-04-26 22:11:35 -0500

    Remove unused definitions

--- main-category-selector.php
+++ main-category-selector.php
@@ -14,11 +14,6 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
 
-// Definitions.
-define( 'MAINCATSEL_VERSION', '0.0.1' );
-define( 'MAINCATSEL_DIR', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
-define( 'MAINCATSEL_URL', untrailingslashit( plugins_url( basename( plugin_dir_path( __FILE__ ) ), basename( __FILE__ ) ) ) );
-
 if ( ! class_exists('MAINCATSEL') ) {
 class MAINCATSEL {
 
